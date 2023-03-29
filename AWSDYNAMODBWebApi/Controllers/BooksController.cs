a18e758b9e5e2e8b2449f6d897c448b7205de7c3
Ramazan Küçükkoç <ramazankucukkoc43@gmail.com>
2023-03-29 21:12:23 +0300

    Add project files.

---
+++ AWSDYNAMODBWebApi/Controllers/BooksController.cs
@@ -0,0 +1,60 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using AWSDYNAMODBWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AWSDYNAMODBWebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+
+        private readonly IDynamoDBContext _dbContext;
+
+        public BooksController(IDynamoDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Getall()
+        {
+            List<Book> books = await _dbContext.ScanAsync<Book>(default).GetRemainingAsync();
+            return Ok(books);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById([FromRoute] string id)
+        {
+            Book book = await _dbContext.LoadAsync<Book>(id);
+            return Ok(book);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] Book book)
+        {
+            //Book getById = await _dbContext.LoadAsync<Book>(book.ID);
+            //if (getById != null)
+            //    return BadRequest($"Book with Id {book.ID} already exists.");
+            await _dbContext.SaveAsync(book);
+            return CreatedAtAction(nameof(BooksController.Add), new { book.Id }, book);
+
+        }
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] Book book)
+        {
+            Book getById = await _dbContext.LoadAsync<Book>(book.Id);
+            if (getById == null)
+                return NotFound();
+            await _dbContext.SaveAsync(book);
+            return Ok();
+        }
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] string id)
+        {
+            Book getById = await _dbContext.LoadAsync<Book>(id);
+            if (getById == null)
+                return NotFound();
+            await _dbContext.DeleteAsync(getById);
+            return Ok();
+        }
+    }
+}
---
+++ AWSDYNAMODBWebApi/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using System.Text.Json.Serialization;
+
+namespace AWSDYNAMODBWebApi.Models
+{
+    [DynamoDBTable("books")]
+    public class Book
+    {
+        [DynamoDBHashKey("id"),JsonPropertyName("id")]
+        public string? Id { get; set; }
+
+        [DynamoDBProperty("name"), JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+    }
+}
